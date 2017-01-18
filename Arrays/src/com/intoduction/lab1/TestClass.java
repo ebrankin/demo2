81c8a5bd4686cae1e320745a9793fd5020e999db
Eamonn <Eamonn@DESKTOP-HL6IELK>
2017-01-18 19:52:29 +0000

    done

--- Arrays/src/com/intoduction/lab1/TestClass.java
+++ Arrays/src/com/intoduction/lab1/TestClass.java
@@ -2,4 +2,17 @@ package com.intoduction.lab1;
 
 public class TestClass {
 
+	public int Max (int a , int b)
+	{
+		if (a > b) { return a;}
+		return b;
+		
+	}
+	
+	public double Max (double a , double b)
+	{
+		if (a > b) { return a;}
+		return b;
+		
+	}
 }
